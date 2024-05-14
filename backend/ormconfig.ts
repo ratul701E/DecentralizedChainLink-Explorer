21365fcb2e42f50892c704b4252efc1a2468e4ba
This is Ratul <newidratul@gmail.com>
2024-05-15 03:07:44 +0600

    new modules added

--- backend/ormconfig.ts
+++ backend/ormconfig.ts
@@ -1,5 +1,9 @@
 
+import { CommentEntity } from 'src/entities/comment.entity';
 import { MessageEntity } from 'src/entities/message.entity';
+import { PostEntity } from 'src/entities/post.entity';
+import { UserEntity } from 'src/entities/user.entity';
+import { VoteEntity } from 'src/entities/vote.entity';
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 
 const config: PostgresConnectionOptions = {
@@ -9,7 +13,7 @@ const config: PostgresConnectionOptions = {
   port: 5432,
   username: 'postgres',
   password: 'postgre',
-  entities: [MessageEntity],
+  entities: [MessageEntity, PostEntity, CommentEntity, UserEntity, VoteEntity],
   synchronize: true,
 };
 
